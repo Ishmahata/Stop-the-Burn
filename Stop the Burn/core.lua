b00544f3b3acdb6859e41cf924ed39d0711a2f02
Ishmahata <46677032+Ishmahata@users.noreply.github.com>
2020-11-23 23:37:56 +0100

    Added comments

--- Stop the Burn/core.lua
+++ Stop the Burn/core.lua
@@ -1,3 +1,6 @@
+--[[ Stop The Burn, a super simple small addon to stop messages in trade/general from a certain EU server that never speaks english
+     Drop the folder in your addons folder. There will be no further updates to this. ]]
+
 local function myChatFilter(self, event, msg, author, ...)
   if author:find("-BurningLegion") then
     return true
@@ -8,3 +11,5 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", myChatFilter)
 ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", myChatFilter)
 -- ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", myChatFilter)
 -- ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", myChatFilter)
+
+-- Uncomment the two rows above if you want to mute YELL and Whispers too
